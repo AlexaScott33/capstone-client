1fdf5394f5a1e0150c502c379f2d6819bd24615b
Alexa Scott <arscott4792@gmail.com>
2018-04-27 11:24:01 -0700

    updated login form to redux form

--- src/actions/users.js
+++ src/actions/users.js
@@ -3,22 +3,6 @@ import { API_BASE_URL } from '../config';
 import { SubmissionError } from 'redux-form';
 import { normalizeResponseErrors } from './utils';
 
-// export const REGISTER_NEW_USER = 'REGISTER_NEW_USER'
-// export const registerNewUser = (user) => ({
-//     type:'REGISTER_NEW_USER',
-//     newUser: user
-// });
-
-// export const REGISTER_NEW_USER_SUCCESS = 'REGISTER_NEW_USER_SUCCESS'
-// export const registerNewUserSuccess = () => ({
-//     type:'CREATE_NEW_USER_SUCCESS'
-// });
-
-
-// export const REGISTER_NEW_USER_ERROR= 'REGISTER_NEW_USER_ERROR'
-// export const registerNewUserError = () => dispatch => ({
-//     type:'REGISTER_NEW_USER_ERROR',
-// });
 
 export const registerUser = (user) => dispatch => {
     return fetch(`${API_BASE_URL}/api/users`, {
--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,30 +1,42 @@
 import React from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+import Input from './input';
 import { withRouter } from 'react-router-dom';
 import { login } from '../actions/auth';
 
 export class LoginForm extends React.Component {
-    onSubmit(values) {
-        const username = values.username.value;
-        const password = values.password.value;
-        console.log(`Logging in as user: ${username}`);
-        return this.props
-            .dispatch(login(username, password))
-            .then(()=>this.props.history.push('/home'))
-    }
+    // onSubmit(values) {
+    //     const username = values.username.value;
+    //     const password = values.password.value;
+    //     console.log(`Logging in as user: ${username}`);
+    //     return this.props
+    //         .dispatch(login(username, password))
+    //         .then(()=>this.props.history.push('/home'))
+    // }
 
     render() {
         return (
             <div>
                 <h3>Existing Users Login</h3>
-                <form onSubmit={(e) => {
-                    e.preventDefault();
-                    this.onSubmit(e.target);
-                }}>
+                <form name="login"
+                onSubmit={this.props.handleSubmit(values => this.props.dispatch(login(values.username, values.password))
+                                                                    .then(()=>this.props.history.push('/home')))}
+                >
                     <label htmlFor="username">UserName</label>
-                    <input type="text" name="username"></input> <br />
+                    <Field 
+                        component={Input} 
+                        type="text"
+                        name="username"
+                        // validate={[required, nonEmpty, isTrimmed]} 
+                    /> <br />
                     <label htmlFor="password">Password</label>
-                    <input type="text" name="password"></input> <br />
+                    <Field 
+                        component={Input} 
+                        type="text" 
+                        name="password"
+                        // validate={[required, passwordLength, isTrimmed]}
+                    /> <br />
                     <button type="submit">Login</button>
                 </form>
             </div>
@@ -32,4 +44,25 @@ export class LoginForm extends React.Component {
     }
 }
 
-export default withRouter(connect()(LoginForm));
+export default withRouter(reduxForm({
+    form: 'login'
+})(LoginForm));
+
+            // <div>
+            //     <h3>Existing Users Login</h3>
+            //     <form onSubmit={(e) => {
+            //         e.preventDefault();
+            //         this.onSubmit(e.target);
+            //     }}>
+            //         <label htmlFor="username">UserName</label>
+            //         <input type="text" name="username"></input> <br />
+            //         <label htmlFor="password">Password</label>
+            //         <input type="text" name="password"></input> <br />
+            //         <button type="submit">Login</button>
+            //     </form>
+            // </div>
+//         )
+//     }
+// }
+
+// export default withRouter(connect()(LoginForm));
