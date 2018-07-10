ae54e2608ba60adac09b6d5b3af9ff49331ded9f
Alexa Scott <arscott4792@gmail.com>
2018-07-10 14:17:56 -0700

    added what button on landing

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,23 +1,69 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Popup from "reactjs-popup";
+
 
 import './landing-page.css';
 import './signup.css';
 
-export default function LandingPage() {
-    return (
-        <div>
-            <header role="banner" className="background">
-                <h1 className="header-landing">WPL Rugby Commentators</h1>
-                <div className="description-container">
-                <h4 className="description-header">Stay up to date with the most recent games played during the Women's Premier League Rugby Season with WPL Rugby Commentators.</h4>
-                <p>New to Commentators? <Link to="/signup" className="link">Sign up</Link> in seconds.</p>
-                <p>Existing users, <Link to="/login" className="link">Login</Link> and welcome back!</p>
-                <p>Once logged in, you can view all of the matches and specific details about the match. Check out the comments section and make sure to post some comments yourself!</p>
-            </div>
-            </header>
-           
-         </div>    
-    );
+export default class LandingPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            toggle: 'landing'
+        }
+    }
+
+    handleClick() {
+        this.setState({
+            toggle: 'what?'
+        });
+    }
+
+    handleClose() {
+        this.setState({
+            toggle: 'landing'
+        });
+    }
+    render() {
+        console.log(this.state.toggle);
+        if(this.state.toggle === 'landing') {
+            return (
+                <div>
+                    <header role="banner" className="background">
+                    <div className="top-container">
+                        <button 
+                        className="landing-button"
+                        onClick={() => this.handleClick()}
+                        >What?</button>
+                        <Link to="/signup" id="link">Sign up</Link>
+                        <Link to="/login" id="link">Login</Link>
+                    </div>
+                        <h1 className="header-landing">WPL Rugby Commentators</h1>
+                        {/* <div className="description-container">
+                        <h4 className="description-header">Stay up to date with the most recent games played during the Women's Premier League Rugby Season with WPL Rugby Commentators.</h4>
+                        <p>Once logged in, you can view all of the matches and specific details about the match. Check out the comments section and make sure to post some comments yourself!</p>
+                    </div> */}
+                    </header>
+                 </div>    
+            );
+        }
+        if(this.state.toggle === 'what?') {
+            return (
+                <div>
+                    <header role="banner" className="toggle-background">
+                    <div className="description-container">
+                        <h4 className="description-header">Stay up to date with the most recent games played during the Women's Premier League Rugby Season with WPL Rugby Commentators.</h4>
+                        <p>Once logged in, you can view all of the matches and specific details about the match. Check out the comments section and make sure to post some comments yourself!</p>
+                        <button
+                    onClick={() => this.handleClose()}
+                    >Close</button>
+                    </div>
+                    
+                       
+                    </header>
+                    
+                </div>
+            );
+        }
+    }
 }
